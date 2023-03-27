36926a7703156b018188b107f4f400f2a53a699c
Pablo Coello <pablofranciscocoello@gmail.com>
2023-03-27 20:37:02 -0300

    optimizacion final del punto_1

--- punto_1.py
+++ punto_1.py
@@ -1,57 +1,9 @@
 def validate_string(string):
-    validate_isalnum(string)
-    validate_isalpha(string)
-    validate_isupper(string)
-    validate_islower(string)
-    validate_isdigit(string)
-    if len(string) >= 8:
-        print(True)
-    else:
-        print(False)
-
-def validate_isalnum(string):
-    list = []
-    for character in string:
-        character = character.isalnum()
-        list.append(character)
-    if True in list:
-        print(True)
-    else:
-        print(False)
-def validate_isalpha(string):
-    list = []
-    for character in string:
-        character = character.isalpha()
-        list.append(character)
-    if True in list:
-        print(True)
-    else:
-        print(False)
-def validate_isupper(string):
-    list = []
-    for character in string:
-        character = character.isupper()
-        list.append(character)
-    if True in list:
-        print(True)
-    else:
-        print(False)
-def validate_islower(string):
-    list = []
-    for character in string:
-        character = character.islower()
-        list.append(character)
-    if True in list:
-        print(True)
-    else:
-        print(False)
-def validate_isdigit(string):
-    list = []
-    for character in string:
-        character = character.isdigit()
-        list.append(character)
-    if True in list:
-        print(True)
-    else:
-        print(False)
+    print(any(character.isalnum() for character in string))
+    print(any(character.isalpha() for character in string))
+    print(any(character.isupper() for character in string))
+    print(any(character.islower() for character in string))
+    print(any(character.isdigit() for character in string))
+    print(len(string) >= 8)
 validate_string("xy@z")
+
