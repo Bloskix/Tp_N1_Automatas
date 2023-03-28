01d5ff5ef98335035310aa67bc7b5951e8b50742
FabricioFernandezz <fabriciofernandezz2001@gmail.com>
2023-03-27 21:01:55 -0300

    testing push

--- Ejercicio_2.py
+++ Ejercicio_2.py
@@ -1,11 +1,17 @@
+def solve(count):
+    add = []
+    numbers = count.split("+")
+    for number in numbers:
+        if number.isdigit():
+            add.append(int(number))
+        else:
+            mult = []
+            mult = number.split("*")
+            result = 1
+            for element in mult:
+                result *= int(element)
+            add.append(result)
+    final_result = sum(add)
+    print(final_result)
 
-def solve(operation):
-    chars = '0123456789+*() '    #si se encuentra un caracter no permitido, se genera un error.
-    if not all(c in chars for c in operation):    # si no cumple com estas condiciones, se genera un error , la letra c toma el valor de cada variable
-        raise ValueError("Operación matemática no válida.")  # raise nos genera un error intencional
-    result = eval(operation) # evaluar una cadena de texto que representa una operación matemática.
-    return result
-print(solve("7 + 5 * 4"))
-print(solve("3+ 3 + 8"))
-print(solve("1+ 5 * 9"))
-
+solve("2*2*2+32*2")
